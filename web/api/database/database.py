b0b3aef2bb850f41d1f34f6ae9a7093aa223abee
AkKiL M G <heimanpicturesofficial@gmail.com>
2024-03-02 11:04:21 +0530

    API: Redis database template

---
+++ web/api/database/database.py
@@ -0,0 +1,15 @@
+
+
+import redis
+
+
+
+class Redis:
+    def __init__(self):
+        self.db = redis.Redis(host='localhost', port=6379, db=0)
+
+    def set(self, key, value):
+        self.r.set(key, value)
+
+    def get(self, key):
+        return self.r.get(key).decode()
