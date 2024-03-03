d6ad344c84773bf73f2156524b6c0f0e910bc144
AkKiL M G <heimanpicturesofficial@gmail.com>
2024-03-03 07:52:21 +0530

    API: Fix

--- web/api/router/crawl_router copy.py
+++ web/api/router/crawl_router copy.py
@@ -1,6 +1,6 @@
 # License: GNU General Public License v3.0
 
-import asyncio, json, subprocess
+import asyncio, json
 from fastapi import APIRouter, WebSocket
 from workers.model import CrawlModel
 from pathlib import Path
@@ -22,12 +22,12 @@ def generate_yara_rule(keywords):
     return rule_template % strings_section
 
 
+
 async def execute_command(command):
     completed_process = await asyncio.create_subprocess_exec(*command)
     await completed_process.wait()
     #completed_process = await asyncio.create_subprocess_shell()
     #await completed_process.wait()
-
 
 async def send_data_to_websocket(websocket):
         while True:
@@ -35,44 +35,43 @@ async def send_data_to_websocket(websocket):
             await websocket.send_text(data)  
             await asyncio.sleep(1)
 
-
-@router.post("/generate_html")
-async def get_html(data: CrawlModel):
+@router.websocket("/generate_html")
+async def websocket_endpoint(websocket: WebSocket):
+        await websocket.accept()
     # try:
-        r = generate_yara_rule(data.keywords)
+        dataRaw = await websocket.receive_text()
+        data: CrawlModel = json.loads(dataRaw)
+        r = generate_yara_rule(data['keywords'])
         file_path = Path(Path(__file__).parent).parent / "torcrawl/res/keywords.yar"
         with open(file_path, 'w') as f:
             f.write(f"{r}\n")
         v, c, d, p, e = True, True, 1, 1, True
         command = [
             'python', 
-            './torcrawl/torcrawl.py', 
+            'torcrawl.py', 
             str(v) and '-v', 
             str(c) and '-c', 
-            '-u', data.url, 
+            '-u', data['url'], 
             '-d', str(d), 
             '-p', str(p), 
             '-o', 'result.txt', 
             str(e) and '-e', 
         ]
-        # subprocess.run(command)
-        
-        # p = subprocess.Popen(" ".join(command), stdout=subprocess.PIPE, shell=True)
-        # (output, err) = p.communicate() 
-        # p_status = p.wait()
-        subprocess.call(command, shell=True)
-
-        return {'success': True, 'message': await _read_file()}
+        subprocess_task = asyncio.create_task(execute_command(command))
+        import time
+        time.sleep(20)
+        file_reading_task = asyncio.create_task(send_data_to_websocket(websocket))
+        await asyncio.gather(subprocess_task, file_reading_task)
     # except Exception as e:
     #     print(f"WebSocket error: {e}")
-        
+    # finally:
+        await websocket.close()
 
 async def _read_file() -> str:
     """Reads contents of the specified file."""
     try:
-        file_path = Path(Path(__file__).parent).parent / "torcrawl/res/keywords.yar"
-        with open(file_path, "r") as file:
-            return file.read()
+        async with open(Path(Path(__file__).parent).parent / "torcrawl/res/keywords.yar", "r") as file:
+            return await file.read()
     except FileNotFoundError:
         print(f"File not found")
         return "Error: File not found"
